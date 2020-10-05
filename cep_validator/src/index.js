77d6c4382c3ea2c4c8f194713f7bdc637c77d57e
fabioNog <fabio.carmo@computacao.ufla.br>
2020-10-05 16:40:02 -0300

    Build component reducer

--- cep_validator/src/index.js
+++ cep_validator/src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
+
+import reducers from './main/reducers'
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+      && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ cep_validator/src/main/reducers.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import { reducer as toastrReducer } from 'react-redux-toastr'
+
+import cepCycleReducer from '../cepCycle/cepReducer'
+
+const rootReducer = combineReducers({
+    cepCycle: cepCycleReducer,
+    form: formReducer,
+    toastr: toastrReducer
+})
+
+export default rootReducer;
