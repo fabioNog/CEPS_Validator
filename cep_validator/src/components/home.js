7bf683361c90e74c14225995c632e029675b3b08
fabioNog <fabio.carmo@computacao.ufla.br>
2020-10-05 13:28:11 -0300

    Add component basic home and add routes for react-router-dom

---
+++ cep_validator/src/components/home.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+
+export default class home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Bem Vindo a Home</h1>
+            </div>
+        )
+    }
+}
