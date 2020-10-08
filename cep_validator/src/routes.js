7367fe6a0cc9b62adb455637827c215e9673cf4d
fabioNog <fabio.carmo@computacao.ufla.br>
2020-10-08 17:22:25 -0300

    Add thunk, middleware, multi,promise

--- cep_validator/src/routes.js
+++ cep_validator/src/routes.js
@@ -5,7 +5,10 @@ import { Provider } from 'react-redux';
 import cityReducer from './cepCycle/reducers/cityReducer';
 import cepReducer from './cepCycle/reducers/cep_Reducer';
 
-import {combineReducers, createStore} from 'redux' 
+import {combineReducers, createStore, applyMiddleware} from 'redux' 
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 
 import {
@@ -17,14 +20,14 @@ import Home from './components/home'
 import Login from './components/login'
 
 
-const reducer = combineReducers({
+const reducers = combineReducers({
   city: cityReducer,
   cep: cepReducer    
 }) 
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(reducer,devTools)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers,devTools)
 
 export default function MainRoutes(){
   return(
