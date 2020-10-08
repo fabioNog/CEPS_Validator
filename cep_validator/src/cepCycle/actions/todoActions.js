615e2bdd68aac1ded14fe60553fccc9f399febd9
fabioNog <fabio.carmo@computacao.ufla.br>
2020-10-08 17:22:46 -0300

    Add todo for get, post

---
+++ cep_validator/src/cepCycle/actions/todoActions.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const URL = 'http://localhost:3003/api/todos'
+
+
+export const search = () => {
+    request = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
+
+export const add = (description) => {
+    return dispatch => {
+        axios.post(URL, { description })
+            .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
+            .then(resp => dispatch(search()))
+    }
+}
+
+
+export const remove = (todo) => {
+    return dispatch => {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => dispatch(search()))
+    }
+}
